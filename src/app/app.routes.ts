9d1e3ab4512bea1072bc39f1021c160e39279cb0
Nilsson-AvH <nlr519.com@gmail.com>
2026-01-22 20:01:54 -0500

    [+] (home,login,register,error-404).ts: Genera componentes de pagina [+] app.routes.ts: Define todas las rutas necesarias [+] (header,fotter).ts: Define componentes layout del site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/pages/home/home';
+import { Login } from './features/pages/login/login';
+import { Register } from './features/pages/register/register';
+import { Error404 } from './features/pages/error-404/error-404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // No olvidar que las rutas deben estar ordenadas de menor a mayor especificidad
+    { path: 'home', component: Home },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'error-404', component: Error404 },
+    // Las redirecciones deben estar al final
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'error-404', pathMatch: 'full' },
+];
