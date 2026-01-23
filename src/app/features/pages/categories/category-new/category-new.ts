30b2d888bda0bfbe7c6aaf5699580dcb0666f2d1
Nilsson-AvH <nlr519.com@gmail.com>
2026-01-22 21:37:57 -0500

    [+] category-new.*: Se capturan los valores del formulario al presionar el boton Register y se muestran en la consola del navegador

--- src/app/features/pages/categories/category-new/category-new.ts
+++ src/app/features/pages/categories/category-new/category-new.ts
@@ -22,4 +22,20 @@ export class CategoryNew {
       description: new FormControl(''),
     })
   }
+
+  // Metodo con el cual vamos a capturar los datos del formulario al presionar el boton submit
+  onSubmit() {
+    // Verificar si el formulario es valido
+    // IMPORTANTE: Si los campos no tienen validaciones, el formulario siempre sera valido
+    if (this.formData.valid) {
+      // Registra haciendo uso del servicio
+
+      // Muestra los datos de los campos del formulario
+      console.log(this.formData.value)
+    }
+    else {
+      // Muestre todos los mensajes de error en la vista de cada uno de los campor
+      console.error('Formulario invalido');
+    }
+  }
 }
