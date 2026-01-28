4b0283cb9f597af155b95128d8ad64cd3c422b09
Nilsson-AvH <nlr519.com@gmail.com>
2026-01-27 21:09:44 -0500

    [+] http-category.ts: define la peticion al backend getCategories() [+] styles: Define CSS para las tablas [+] category-list.*: obtiene los datos del servicio

--- src/app/core/services/http-category.ts
+++ src/app/core/services/http-category.ts
@@ -1,15 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// El servicio del frontend se habla con el backend por medio de la url base
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpCategory {
+  private base_url: string = 'http://localhost:3000/api/categories';
   // const http = inject(HttpClient); // Inyección de dependencia en una funcion
   // Se inyecta como dependencia la clase que permite hacer peticiones HTTP
   constructor(private http: HttpClient) { }
 
-  createCategory(newCategory: any) {
-    return this.http.post('http://localhost:3000/api/categories', newCategory);
+  createCategory(newCategory: any): Observable<any> {
+    return this.http.post(this.base_url, newCategory);
   }
+  getCategories(): Observable<any> {
+    return this.http.get(this.base_url);
+  }
+  // Verificar que estructura de datos me trae el backend
+  // getCategories().pipe
+  // }
 }
--- src/app/features/pages/categories/category-list/category-list.ts
+++ src/app/features/pages/categories/category-list/category-list.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HttpCategory } from '../../../../core/services/http-category';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-category-list',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './category-list.html',
   styleUrl: './category-list.css',
+  changeDetection: ChangeDetectionStrategy.OnPush // mi app usa single change detection pero se puede cambiar a Default
 })
 export class CategoryList {
+  // Definir el atributo que contendra la estructura de las categorias
+  // categories: any[] = []; // Signals
+  public categories$: Observable<any[]> = new Observable<any[]>();
 
+  constructor(private httpCategory: HttpCategory) { }
+
+  // Usamos el Hook del ciclo de vida del componente
+  ngOnInit() {
+    this.categories$ = this.httpCategory.getCategories();
+  }
+
+  // Usamos el Hook del Ciclo de vida que avisa que se esta inicializando el componente
+  // ngOnInit() {
+  //   this.categories$ = this.httpCategory.getCategories()
+  // IMPORTANTE: Si la estructura de datos que Ud esta trayendo del Backend tiene varias propiedades, asegurece de extraer solo los datos que necesita
+  // .pipe(
+  //   map( data => {
+  //     return data.data;
+  //   }),
+  //   catchError( err => {
+  //     return of([])
+  //   })
+  // );
 }
+
